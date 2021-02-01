7a909bab7d13af041519fe386572ce228f88aa26
gregoryord <g.ord@hotmail.ca>
2021-02-01 07:53:41 -0700

    Moved definitions of the core build sequence steps
    to CoreBuildSequence.h

--- sw/core/main/BuildSequence.c
+++ sw/core/main/BuildSequence.c
@@ -1,13 +1,9 @@
 #include "BuildSequence.h"
 
 #include "../../external/GregCToolkit/sw/CommandLineOptions/CommandLineOptions_ll.h"
-#include "../application/CompileAndLinkCommands.h"
-#include "../application/RunTests.h"
 #include "../common/BuildSequenceStep.h"
 #include "../common/GregBuildConstants.h"
-#include "../fileSystemRecursion/FileAndTestCaseGatherer.h"
-#include "../fileSystemRecursion/FileOperations.h"
-#include "../testMainWriting/TestMainWriter.h"
+#include "CoreBuildSequence.h"
 
 void initBuildSequence(LinkedList* sequence)
 {
@@ -17,48 +13,13 @@ void initBuildSequence(LinkedList* sequence)
 
 void setCoreBuildSequenceSteps(LinkedList* sequence)
 {
-   BuildSequenceStep* loadTestsAndSourceFilesStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       loadTestsAndSourceFilesStep, NULL_COMMAND_LINE_DESCRIPTION, NULL_COMMAND_LINE_OPTION_TEXT, NULL_COMMAND_LINE_FLAG_VALUE, loadTestsAndSourceFiles,
-       "loadTestsAndSourceFiles");
-   append_ll(sequence, loadTestsAndSourceFilesStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* compileIntoTempObjectFilesStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       compileIntoTempObjectFilesStep, NULL_COMMAND_LINE_DESCRIPTION, NULL_COMMAND_LINE_OPTION_TEXT, NULL_COMMAND_LINE_FLAG_VALUE, compileIntoTempObjectFiles,
-       "compileIntoTempObjectFiles");
-   append_ll(sequence, compileIntoTempObjectFilesStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* linkObjectFilesWithGregTestDllToMakeProjectTestDllStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       linkObjectFilesWithGregTestDllToMakeProjectTestDllStep, NO_TEST_DESCRIPTION, NO_TEST_OPTION_TEXT, NO_TEST_FLAG_VALUE,
-       linkObjectFilesWithGregTestDllToMakeProjectTestDll, "linkObjectFilesWithGregTestDllToMakeProjectTestDll");
-   append_ll(sequence, linkObjectFilesWithGregTestDllToMakeProjectTestDllStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* writeTestsToTestMainStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(writeTestsToTestMainStep, NO_TEST_DESCRIPTION, NO_TEST_OPTION_TEXT, NO_TEST_FLAG_VALUE, writeTestsToTestMain, "writeTestsToTestMain");
-   append_ll(sequence, writeTestsToTestMainStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* createTestMainExecutableFromProjectDllAndGregTestDllStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       createTestMainExecutableFromProjectDllAndGregTestDllStep, NO_TEST_DESCRIPTION, NO_TEST_OPTION_TEXT, NO_TEST_FLAG_VALUE,
-       createTestMainExecutableFromProjectDllAndGregTestDll, "createTestMainExecutableFromProjectDllAndGregTestDll");
-   append_ll(sequence, createTestMainExecutableFromProjectDllAndGregTestDllStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* runTestsWithExitStatusCheckStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       runTestsWithExitStatusCheckStep, NO_TEST_DESCRIPTION, NO_TEST_OPTION_TEXT, NO_TEST_FLAG_VALUE, runTestsWithExitStatusCheck, "runTestsWithExitStatusCheck");
-   append_ll(sequence, runTestsWithExitStatusCheckStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* compileObjectFilesIntoProjectExecutableStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(
-       compileObjectFilesIntoProjectExecutableStep, NULL_COMMAND_LINE_DESCRIPTION, NULL_COMMAND_LINE_OPTION_TEXT, NULL_COMMAND_LINE_FLAG_VALUE,
-       compileObjectFilesIntoProjectExecutable, "compileObjectFilesIntoProjectExecutable");
-   append_ll(sequence, compileObjectFilesIntoProjectExecutableStep, BUILD_SEQUENCE_STEP_TYPE);
-
-   BuildSequenceStep* removeTempDirStep = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
-   allocateAndSetBuildSequenceStep(removeTempDirStep, DELETE_TEMP_DIR_DESCRIPTION, DELETE_TEMP_DIR_OPTION_TEXT, DELETE_TEMP_DIR_FLAG_VALUE, removeTempDir, "removeTempDir");
-   append_ll(sequence, removeTempDirStep, BUILD_SEQUENCE_STEP_TYPE);
+   for (int i = 0; i < (sizeof(coreBuildSequenceInfo) / sizeof(BuildSequenceStepInfo)); i++)
+   {
+      BuildSequenceStep* step = (BuildSequenceStep*)malloc(sizeof(BuildSequenceStep));
+      BuildSequenceStepInfo info = coreBuildSequenceInfo[i];
+      allocateAndSetBuildSequenceStep(step, info.description, info.optionText, info.flagValue, info.function_ptr, info.functionName);
+      append_ll(sequence, step, BUILD_SEQUENCE_STEP_TYPE);
+   }
 }
 
 int executeBuildSequence(LinkedList* buildSequence, LinkedList* options, TestFileList* testFiles, SourceFileList* sourceFiles, ObjectFileList* tempObjectFiles)
---
+++ sw/core/main/CoreBuildSequence.h
@@ -0,0 +1,81 @@
+#ifndef CORE_BUILD_SEQUENCE_H
+#define CORE_BUILD_SEQUENCE_H
+
+#include "../application/CompileAndLinkCommands.h"
+#include "../common/BuildSequenceStep.h"
+#include "../application/RunTests.h"
+#include "../common/GregBuildConstants.h"
+#include "../fileSystemRecursion/FileOperations.h"
+#include "../fileSystemRecursion/FileAndTestCaseGatherer.h"
+#include "../testMainWriting/TestMainWriter.h"
+
+typedef struct BuildSequenceStepInfo
+{
+    const char* description;
+    const char* optionText;
+    const bool* flagValue;
+    void* function_ptr;
+    const char* functionName;
+} BuildSequenceStepInfo;
+
+BuildSequenceStepInfo coreBuildSequenceInfo[8] = 
+{
+    {
+        NULL_COMMAND_LINE_DESCRIPTION, 
+        NULL_COMMAND_LINE_OPTION_TEXT, 
+        (bool*)NULL_COMMAND_LINE_FLAG_VALUE, 
+        loadTestsAndSourceFiles, 
+        "loadTestsAndSourceFiles"
+    },
+    {
+        NULL_COMMAND_LINE_DESCRIPTION, 
+        NULL_COMMAND_LINE_OPTION_TEXT, 
+        (bool*)NULL_COMMAND_LINE_FLAG_VALUE, 
+        compileIntoTempObjectFiles, 
+        "compileIntoTempObjectFiles"
+    },
+    {
+        NO_TEST_DESCRIPTION, 
+        NO_TEST_OPTION_TEXT, 
+        (bool*)NO_TEST_FLAG_VALUE, 
+        linkObjectFilesWithGregTestDllToMakeProjectTestDll, 
+        "linkObjectFilesWithGregTestDllToMakeProjectTestDll"
+    },
+    {
+        NO_TEST_DESCRIPTION, 
+        NO_TEST_OPTION_TEXT, 
+        (bool*)NO_TEST_FLAG_VALUE, 
+        writeTestsToTestMain, 
+        "writeTestsToTestMain"
+    },
+    {
+        NO_TEST_DESCRIPTION, 
+        NO_TEST_OPTION_TEXT, 
+        (bool*)NO_TEST_FLAG_VALUE, 
+        createTestMainExecutableFromProjectDllAndGregTestDll, 
+        "createTestMainExecutableFromProjectDllAndGregTestDll"
+    }, 
+    {
+        NO_TEST_DESCRIPTION, 
+        NO_TEST_OPTION_TEXT, 
+        (bool*)NO_TEST_FLAG_VALUE, 
+        runTestsWithExitStatusCheck, 
+        "runTestsWithExitStatusCheck"
+    },
+    {
+        NULL_COMMAND_LINE_DESCRIPTION, 
+        NULL_COMMAND_LINE_OPTION_TEXT, 
+        (bool*)NULL_COMMAND_LINE_FLAG_VALUE, 
+        compileObjectFilesIntoProjectExecutable, 
+        "compileObjectFilesIntoProjectExecutable"
+    },
+    {
+        DELETE_TEMP_DIR_DESCRIPTION, 
+        DELETE_TEMP_DIR_OPTION_TEXT, 
+        (bool*)DELETE_TEMP_DIR_FLAG_VALUE, 
+        removeTempDir, 
+        "removeTempDir"
+    }
+};
+
+#endif
