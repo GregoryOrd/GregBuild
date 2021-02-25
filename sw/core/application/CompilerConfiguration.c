7adb39d2a95e1e3942910c6764b6dafeb0dba6f7
gregoryord <g.ord@hotmail.ca>
2021-02-25 08:28:58 -0700

    First commit towards using hash table for storing SetConfiguration structs
    This is working in the ExampleSrcRepo which has only host and target compiler configuration in the current
    config file. Failing in the Blink repo which has more things in its compiler.config.

--- sw/core/application/CompilerConfiguration.c
+++ sw/core/application/CompilerConfiguration.c
@@ -5,11 +5,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../../external/GregCToolkit/sw/Collections/HashTable/HashTable.h"
 #include "../../external/GregCToolkit/sw/FileIO/FileReader.h"
 #include "../../external/GregCToolkit/sw/String/StringUtils.h"
 #include "../common/FileStructureDefs.h"
 #include "../common/GregBuildConstants.h"
 #include "CompilerConfigurationValues.h"
+
+//////////////////////////////////////////////////////////////////////
+//              Private Data and Function Prototypes                //
+//////////////////////////////////////////////////////////////////////
+
+#define NUM_COMPILER_CONFIG_PARAMS    8
+#define SET_CONFIGURATION_TYPE        1
+#define actionPerConfigurationSetting 2
+#define dataMembersPerAction          1
+
+void setDataToActOn(int i);
+HashTable* setupConfigurationsHashTable();
+int string_copy(void** args);
+void freeSetupConfigurations();
+
+typedef int (*SET_CONFIGURATION_ACTION)(void**);
+typedef struct SetConfiguration
+{
+   SET_CONFIGURATION_ACTION actions[actionPerConfigurationSetting];
+   void* dataToActOn[dataMembersPerAction * actionPerConfigurationSetting];
+} SetConfiguration;
+
+const char* compilerConfigParams[NUM_COMPILER_CONFIG_PARAMS] = {"host",         "target",           "compilerOption",    "hostCompilerOption", "targetCompilerOption",
+                                                                "linkerOption", "hostLinkerOption", "targetLinkerOption"};
+
+SetConfiguration configurations[NUM_COMPILER_CONFIG_PARAMS] = {
+    {.actions = {string_copy, NULL}, .dataToActOn = {hostCompiler_, NULL}},                            // host
+    {.actions = {string_copy, NULL}, .dataToActOn = {targetCompiler_, NULL}},                          // target
+    {.actions = {append_string_voidArgs_ll, append_string_voidArgs_ll}, .dataToActOn = {NULL, NULL}},  // compilerOption
+    {.actions = {append_string_voidArgs_ll, NULL}, .dataToActOn = {NULL}},                             // hostCompilerOption
+    {.actions = {append_string_voidArgs_ll, NULL}, .dataToActOn = {NULL}},                             // targetCompilerOption
+    {.actions = {append_string_voidArgs_ll, append_string_voidArgs_ll}, .dataToActOn = {NULL, NULL}},  // compilerOption
+    {.actions = {append_string_voidArgs_ll, NULL}, .dataToActOn = {NULL}},                             // hostLinkerOption
+    {.actions = {append_string_voidArgs_ll, NULL}, .dataToActOn = {NULL}}                              // targetLinkerOption
+};
+
+//////////////////////////////////////////////////////////////////////
+//              Function Implementation Section                     //
+//////////////////////////////////////////////////////////////////////
 
 int readCompilerConfigurationFromFile()
 {
@@ -93,40 +133,132 @@ void thisAction(void** args)
 */
 void setConfigurations(const char* param, const char* value)
 {
-   if (strcmp(param, "host") == 0)
+   HashTable* table = setupConfigurationsHashTable();
+   for (int i = 0; i < NUM_COMPILER_CONFIG_PARAMS; i++)
    {
-      strcpy(hostCompiler_, value);
+      SetConfiguration* setConfiguration = (SetConfiguration*)hash_lookup(table, param, SET_CONFIGURATION_TYPE);
+      for (int actionNum = 0; actionNum < actionPerConfigurationSetting; actionNum++)
+      {
+         SET_CONFIGURATION_ACTION action = setConfiguration->actions[actionNum];
+         if (action == NULL)
+         {
+            continue;
+         }
+         for (int dataNum = 0; dataNum < actionPerConfigurationSetting * dataMembersPerAction; dataNum++)
+         {
+            void* data = setConfiguration->dataToActOn[dataNum];
+            if (data == NULL)
+            {
+               continue;
+            }
+
+            void* args[3];
+            args[0] = data;
+            args[1] = (void*)value;
+            int listType = COMPILER_OPTION_TYPE;
+            if (action == append_string_voidArgs_ll && i < 2 && i > 4)
+            {
+               listType = LINKER_OPTION_TYPE;
+            }
+            args[2] = &listType;
+            action(args);
+         }
+      }
    }
-   else if (strcmp(param, "target") == 0)
+
+   // if (strcmp(param, "host") == 0)
+   // {
+   //    strcpy(hostCompiler_, value);
+   // }
+   // else if (strcmp(param, "target") == 0)
+   // {
+   //    strcpy(targetCompiler_, value);
+   // }
+   // else if (strcmp(param, "compilerOption") == 0)
+   // {
+   //    append_string_ll(hostCompilerOptions_, value, COMPILER_OPTION_TYPE);
+   //    append_string_ll(targetCompilerOptions_, value, COMPILER_OPTION_TYPE);
+   // }
+   // else if (strcmp(param, "hostCompilerOption") == 0)
+   // {
+   //    append_string_ll(hostCompilerOptions_, value, COMPILER_OPTION_TYPE);
+   // }
+   // else if (strcmp(param, "targetCompilerOption") == 0)
+   // {
+   //    append_string_ll(targetCompilerOptions_, value, COMPILER_OPTION_TYPE);
+   // }
+   // else if (strcmp(param, "linkerOption") == 0)
+   // {
+   //    append_string_ll(hostLinkerOptions_, value, LINKER_OPTION_TYPE);
+   //    append_string_ll(targetLinkerOptions_, value, LINKER_OPTION_TYPE);
+   // }
+   // else if (strcmp(param, "hostLinkerOption") == 0)
+   // {
+   //    append_string_ll(hostLinkerOptions_, value, LINKER_OPTION_TYPE);
+   // }
+   // else if (strcmp(param, "targetLinkerOption") == 0)
+   // {
+   //    append_string_ll(targetLinkerOptions_, value, LINKER_OPTION_TYPE);
+   // }
+   freeHashTable(table, freeSetupConfigurations, false, false);
+}
+
+void freeSetupConfigurations() {}
+
+HashTable* setupConfigurationsHashTable()
+{
+   HashTable* table = malloc(sizeof(HashTable));
+   initHashTable(table, SET_CONFIGURATION_TYPE, NUM_COMPILER_CONFIG_PARAMS);
+   for (int i = 0; i < NUM_COMPILER_CONFIG_PARAMS; i++)
    {
-      strcpy(targetCompiler_, value);
+      setDataToActOn(i);
+      HashTableItem* item = malloc(sizeof(HashTableItem));
+      item->key = compilerConfigParams[i];
+      item->data = &configurations[i];
+      hash_insert(table, item, SET_CONFIGURATION_TYPE);
    }
-   else if (strcmp(param, "compilerOption") == 0)
+   return table;
+}
+
+void setDataToActOn(int i)
+{
+   void* firstData = configurations[i].dataToActOn[0];
+   void* secondData = configurations[i].dataToActOn[0];
+   switch (i)
    {
-      append_string_ll(hostCompilerOptions_, value, COMPILER_OPTION_TYPE);
-      append_string_ll(targetCompilerOptions_, value, COMPILER_OPTION_TYPE);
+      case 2:  // compilerOption
+         firstData = (void*)hostCompilerOptions_;
+         secondData = (void*)targetCompilerOptions_;
+         break;
+      case 3:  // hostCompilerOption
+         firstData = (void*)hostCompilerOptions_;
+         break;
+      case 4:  // targetCompilerOption
+         firstData = (void*)hostCompilerOptions_;
+         break;
+      case 5:  // linkerOption
+         firstData = (void*)hostLinkerOptions_;
+         secondData = (void*)targetLinkerOptions_;
+         break;
+      case 6:  // hostLinkerOption
+         firstData = (void*)hostLinkerOptions_;
+         break;
+      case 7:  // targetLinkerOption
+         firstData = (void*)targetLinkerOptions_;
+         break;
    }
-   else if (strcmp(param, "hostCompilerOption") == 0)
-   {
-      append_string_ll(hostCompilerOptions_, value, COMPILER_OPTION_TYPE);
-   }
-   else if (strcmp(param, "targetCompilerOption") == 0)
-   {
-      append_string_ll(targetCompilerOptions_, value, COMPILER_OPTION_TYPE);
-   }
-   else if (strcmp(param, "linkerOption") == 0)
-   {
-      append_string_ll(hostLinkerOptions_, value, LINKER_OPTION_TYPE);
-      append_string_ll(targetLinkerOptions_, value, LINKER_OPTION_TYPE);
-   }
-   else if (strcmp(param, "hostLinkerOption") == 0)
-   {
-      append_string_ll(hostLinkerOptions_, value, LINKER_OPTION_TYPE);
-   }
-   else if (strcmp(param, "targetLinkerOption") == 0)
-   {
-      append_string_ll(targetLinkerOptions_, value, LINKER_OPTION_TYPE);
-   }
+}
+
+int string_copy(void** args)
+{
+   char* configurationValue = (char*)args[0];
+   printf("%s\n", configurationValue);
+   const char* value = (const char*)args[1];
+   printf("Value: %s\n", value);
+   strcpy(configurationValue, value);
+   // printf("Copied into %s\n", configurationValue);
+   printf("=============================================\n");
+   return 0;
 }
 
 void initOptionLists()
