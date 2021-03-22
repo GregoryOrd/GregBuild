a5f4a808e2d06decf095df489dda6ecc2e2367ef
gregoryord <g.ord@hotmail.ca>
2021-03-22 08:28:12 -0600

    Adding asserts in GregTest

--- ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld.c
+++ ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld.c
@@ -26,3 +26,27 @@ void testIntegers()
 
    G_ASSERT_INT_EQ(a, b);
 }
+
+void testChars()
+{
+   char a = 'a';
+   char b = 'a';
+
+   G_ASSERT_CHAR_EQ(a, b);
+}
+
+void testUnsignedChar()
+{
+   unsigned char a = 123;
+   unsigned char b = 123;
+
+   G_ASSERT_UNSIGNED_CHAR_EQ(a, b);
+}
+
+void testSignedChar()
+{
+   signed char a = 123;
+   signed char b = 123;
+
+   G_ASSERT_SIGNED_CHAR_EQ(a, b);
+}
