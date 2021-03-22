988cb23e637fe480e843c29239791913d4bd6575
gregoryord <g.ord@hotmail.ca>
2021-03-22 06:56:31 -0600

    GregTest added assertion for int

--- ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld.c
+++ ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld.c
@@ -1,18 +1,28 @@
-#include "../HelloWorld.h"
-#include "../../GregTest.h"
+#include <GregTest.h>
 #include <stdio.h>
+
+#include "../HelloWorld.h"
 
 void testMySt456uff()
 {
-	const char* expected = "Hello World";
-	const char* actual = "Hello World";
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-	G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
 
-void testMySt456f(){
-	const char* expected = "Hello World";
-	const char* actual = "Hello World";
+void testMySt456f()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-	G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
+}
+
+void testIntegers()
+{
+   int a = 5;
+   int b = 5;
+
+   G_ASSERT_INT_EQ(a, b);
 }
--- ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld2.c
+++ ExampleSrcRepo/src/HelloWorld/testHelloWorld/TestHelloWorld2.c
@@ -1,50 +1,52 @@
+#include <GregTest.h>
 #include <stdio.h>
 
-#include "../../GregTest.h"
 #include "../HelloWorld.h"
 
-void testHelloWorldStringssssssssssssssssss1231ssttttttttt(){
+void testHelloWorldStringssssssssssssssssss1231ssttttttttt()
+{
    const char* expected = "Hello World";
    const char* actual = "Hello World";
 
    G_ASSERT_STR_EQ(expected, actual);
 }
 
-void testHelloWorldStringsssssssssssssssssssstttttttttldldldldgfdfkgv2132jaspodc(){
-    const char* expected = "Hello World";
-    const char* actual = "Hello World";
+void testHelloWorldStringsssssssssssssssssssstttttttttldldldldgfdfkgv2132jaspodc()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-    G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
 
+void testHelloWorldStringsssssssssssssssssssstttttttttldldld5321ldgfdfkgvjaspodc()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-void testHelloWorldStringsssssssssssssssssssstttttttttldldld5321ldgfdfkgvjaspodc(){
-    const char* expected = "Hello World";
-    const char* actual = "Hello World";
-
-    G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
 
+void testHelloWorldStringsssssssssssssssssssstttttttttldldsdfgdhfdghldldg4856fdfkgvjaspodc()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-void testHelloWorldStringsssssssssssssssssssstttttttttldldsdfgdhfdghldldg4856fdfkgvjaspodc(){
-    const char* expected = "Hello World";
-    const char* actual = "Hello World";
-
-    G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
 
+void testHelloWorldStringsssssssssssssssssssstttttttttldldldldsafd48566dsfggfdfkgvjaspodc()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-void testHelloWorldStringsssssssssssssssssssstttttttttldldldldsafd48566dsfggfdfkgvjaspodc(){
-    const char* expected = "Hello World";
-    const char* actual = "Hello World";
-
-    G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
 
+void testHelloWorldStringsssssssssssssssssssstttttttt864532tldldldldgfdfksadfsdfggvjaspodc()
+{
+   const char* expected = "Hello World";
+   const char* actual = "Hello World";
 
-void testHelloWorldStringsssssssssssssssssssstttttttt864532tldldldldgfdfksadfsdfggvjaspodc(){
-    const char* expected = "Hello World";
-    const char* actual = "Hello World";
-
-    G_ASSERT_STR_EQ(expected, actual);
+   G_ASSERT_STR_EQ(expected, actual);
 }
